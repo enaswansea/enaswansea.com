2f18775b80a6bdca9c55bb1ef71fa65112fd3d81
Dan Taeyoung <you@example.com>
2019-01-28 12:54:34 -0500

    moving kirby site to subdirectory

---
+++ kirbysite/site/accounts/testuser.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: testuser
+password: >
+  $2y$10$ubux2VnqtgP5TroDvonq.eXjFMmmNpa3emNeNdyLcuAhf0LS/ACy.
+email: test@example.com
+language: en
+role: admin
+history:
+  - bio
+  - home
+  - gallery
+  - gallery/cyberworlds
+  - gallery/magic-mountains
