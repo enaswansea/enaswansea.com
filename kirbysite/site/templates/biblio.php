bbeb5cd571692c6dcd4f502704fa2985f2a7dcec
Dan Taeyoung <you@example.com>
2019-02-18 15:22:22 -0500

    biblio changes

--- kirbysite/site/templates/biblio.php
+++ kirbysite/site/templates/biblio.php
@@ -5,20 +5,24 @@ $biblio = page('biblio')->children()->visible();
 
   <main class="main" role="main">
 
+    <div class="text wrap">
+      
+      <?= $page->text()->kirbytext() ?>
+    </div>
+
+
 <ul class="showcase grid gutter-1">
 
   <?php foreach($biblio as $article): ?>
 
-    <li class="showcase-item column">
+    <div class="showcase-item column">
         <a href="<?= $article->url() ?>" class="showcase-link">
           <?php if($image = $article->images()->sortBy('sort', 'asc')->first()): $thumb = $image->crop(600, 600); ?>
             <img src="<?= $thumb->url() ?>" alt="Thumbnail for <?= $article->title()->html() ?>" class="showcase-image" />
           <?php endif ?>
-          <div class="showcase-caption">
-            <h3 class="showcase-title"><?= $article->title()->html() ?></h3>
-          </div>
+          <div class="showcase-summary"><?= $article->summary()->html() ?></div>
         </a>
-    </li>
+    </div>
 
   <?php endforeach ?>
 
